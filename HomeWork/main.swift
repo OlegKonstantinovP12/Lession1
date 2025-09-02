903639364f2a9b97cb045b2c070c863fbf7526b3
Oleg Konstantinov <oleg.konstantinov.it@gmail.com>
2025-09-02 13:03:44 +0200

    Study interpolation

--- HomeWork/main.swift
+++ HomeWork/main.swift
@@ -34,3 +34,30 @@ let countNumbers: Int = firstNumber + secondNumber
 
 print("Сумма чисел " + String(firstNumber) + " и " + String(secondNumber) + " равна " + String(countNumbers))
 
+//MARK: - Interpolation
+//Задание 1
+message = "Меня зовут \(fullName), и мне \(age) года"
+
+//Задание 2
+var weight: Int = 0
+var height: Double = 0
+print("Введите Ваш вес:")
+weight = Int(readLine() ?? "0") ?? 0
+
+print("Введите Ваш рост в сантиметрах")
+height = Double(readLine() ?? "0") ?? 0
+
+let resultIMT: Double = Double(weight) / pow(height / 100, 2)
+
+print("Ваш ИМТ равен \(resultIMT)")
+
+//Задание 3
+
+var productName: String = "Молоко"
+var price: Int = 100
+var quantity: Int = 3
+
+let shoppingCart = "Вы добавили в корзину \(quantity) шт. товара '\(productName)' на сумму \(price * quantity) руб."
+
+print(shoppingCart)
+
