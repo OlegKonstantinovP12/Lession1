6c7e8abd4199fa8f74c89b57f4926ce36fcc0ca3
Oleg Konstantinov <oleg.konstantinov.it@gmail.com>
2025-09-02 12:51:13 +0200

    Initial Commit

---
+++ HomeWork/main.swift
@@ -0,0 +1,36 @@
+//
+//  main.swift
+//  HomeWork
+//
+//  Created by Oleg Konstantinov on 02.09.2025.
+//
+
+import Foundation
+
+//MARK: - Конкатенация
+// Задание 1
+let firstName: String = "Oleg"
+let lastName: String = "Konstantinov"
+let fullName: String = firstName + " " + lastName
+
+print(fullName)
+
+//Задание 2
+var age: Int = 33
+
+var message: String = "Меня зовут " + fullName + ", и мне " + String(age) + " года"
+
+//Задание 3
+var firstNumber: Int = 0
+var secondNumber: Int = 0
+
+print("Введите первое число")
+firstNumber = Int(readLine() ?? "0") ?? 0
+
+print("Введите второе число")
+secondNumber = Int(readLine() ?? "0") ?? 0
+
+let countNumbers: Int = firstNumber + secondNumber
+
+print("Сумма чисел " + String(firstNumber) + " и " + String(secondNumber) + " равна " + String(countNumbers))
+
